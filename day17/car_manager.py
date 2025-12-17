8bed49d33e2f5e8bcb99aa1abd33940165c940f2
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2025-12-17 22:45:23 +0000

    Day 17 turtle crossing game

---
+++ day17/car_manager.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager():
+
+    def __init__(self):
+        self.cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+
+    def create_random_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            car = Turtle("square")
+            car.penup()
+            car.shapesize(1, 2)
+            car.color(random.choice(COLORS))
+            rand_y = random.randint(-250, 250)
+            car.goto(300, rand_y)
+            self.cars.append(car)
+
+
+    def move_cars(self):
+        for car in self.cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
